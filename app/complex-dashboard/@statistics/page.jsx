b6380d31d5d4a74637facf08ce24299e6e48b794
SZoxid <zoxidsiddikov@gmail.com>
2024-08-02 22:30:00 +0500

    new commit

---
+++ app/complex-dashboard/@statistics/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function StatisticsPage() {
+  return (
+    <div>
+      <h2>Statistics page</h2>
+    </div>
+  )
+}
---
+++ app/complex-dashboard/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ComplexDashboardPage() {
+  return (
+    <div>
+      <h1>ComplexDashboard page</h1>
+    </div>
+  )
+}
